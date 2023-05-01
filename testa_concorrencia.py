676d5076ced4439cefa3e4f9fe511f88df5970b6
Danton Issler Rodrigues <danton.issler18@gmail.com>
2023-05-01 19:06:27 -0400

    Tratamento de exceções e boas praticas.

---
+++ testa_concorrencia.py
@@ -0,0 +1,9 @@
+arquivo1 = open('dados/contatos-escrita.csv', encoding='latin_1', mode='w')
+arquivo2 = open('dados/contatos-escrita.csv', encoding='latin_1', mode='w')
+
+
+contato_carol = '11,Carol,carol@carol.com.br\n'
+contato_andreza = '12,Andreza,andreza@andreza.com.br\n'
+
+arquivo1.write(contato_carol)
+arquivo2.write(contato_andreza)
